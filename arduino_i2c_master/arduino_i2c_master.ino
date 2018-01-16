28a54af11e139a144115d67d1efa25f51dcb6502
asuki <asukiaaa@gmail.com>
2018-01-16 21:58:23 +0900

    Update test i2c signal on arduino

--- arduino_i2c_master/arduino_i2c_master.ino
+++ arduino_i2c_master/arduino_i2c_master.ino
@@ -12,6 +12,13 @@ void setup() {
   delay(500);
 }
 
+void writeByte(char deviceAddress, char bufferAddress, char data) {
+  Wire.beginTransmission(deviceAddress);
+  Wire.write(bufferAddress);
+  Wire.write(data);
+  Wire.endTransmission();
+}
+
 void read3bytesState() {
   Wire.beginTransmission(I2C_SLAVE_ADDRESS);
   Wire.write(R_ADDRESS);
@@ -26,22 +33,65 @@ void read3bytesState() {
   Serial.println("");
 }
 
+void read1byteState() {
+  Wire.beginTransmission(I2C_SLAVE_ADDRESS);
+  Wire.write(RGB_ADDRESS);
+  Wire.endTransmission();
+  Wire.requestFrom(I2C_SLAVE_ADDRESS, 1);
+  delay(10);
+  Serial.print("read: ");
+  while (Wire.available()) {
+    Serial.print(Wire.read(), BIN);
+  }
+  Serial.println("");
+}
+
 void loop() {
-  Wire.beginTransmission(I2C_SLAVE_ADDRESS);
-  Wire.write(G_ADDRESS);
-  Wire.write(0x01);
-  Wire.endTransmission();
-  delay(1000);
-
+  Serial.println("turn R on");
+  writeByte(I2C_SLAVE_ADDRESS, R_ADDRESS, 1);
   read3bytesState();
+  read1byteState();
   delay(1000);
 
-  Wire.beginTransmission(I2C_SLAVE_ADDRESS);
-  Wire.write(G_ADDRESS);
-  Wire.write(0);
-  Wire.endTransmission();
-  delay(1000);
-
+  Serial.println("turn G on");
+  writeByte(I2C_SLAVE_ADDRESS, G_ADDRESS, 1);
   read3bytesState();
+  read1byteState();
+  delay(1000);
+
+  Serial.println("turn B on");
+  writeByte(I2C_SLAVE_ADDRESS, B_ADDRESS, 1);
+  read3bytesState();
+  read1byteState();
+  delay(1000);
+
+  Serial.println("turn R off");
+  writeByte(I2C_SLAVE_ADDRESS, R_ADDRESS, 0);
+  read3bytesState();
+  read1byteState();
+  delay(1000);
+
+  Serial.println("R:on,  G:on,  B:on");
+  writeByte(I2C_SLAVE_ADDRESS, RGB_ADDRESS, 0b111);
+  read3bytesState();
+  read1byteState();
+  delay(1000);
+
+  Serial.println("R:off, G:on,  B:on");
+  writeByte(I2C_SLAVE_ADDRESS, RGB_ADDRESS, 0b011);
+  read3bytesState();
+  read1byteState();
+  delay(1000);
+
+  Serial.println("R:off, G:off, B:on");
+  writeByte(I2C_SLAVE_ADDRESS, RGB_ADDRESS, 0b001);
+  read3bytesState();
+  read1byteState();
+  delay(1000);
+
+  Serial.println("R:off, G:off, B:off");
+  writeByte(I2C_SLAVE_ADDRESS, RGB_ADDRESS, 0);
+  read3bytesState();
+  read1byteState();
   delay(1000);
 }
